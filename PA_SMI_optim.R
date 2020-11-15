0220566cf04a899e0609137ce89ddbc2a47547aa
Pascal Aigner <pascal.aigner97@gmail.com>
2020-11-15 10:43:56 +0100

    updated

--- PA_SMI_optim.R
+++ PA_SMI_optim.R
@@ -6,5 +6,5 @@ library(ggcorrplot)
 # Get code directory
 dir = "."
 # Load data and functions
-load(paste0(dir, "/Data/SMI_returns_1d.Rda"))
-R.utils::sourceDirectory(paste0(dir, "/Functions_optim"), modifiedOnly = F)
+load(paste0(dir, "/Data/returns_1d.Rda"))
+R.utils::sourceDirectory(paste0(dir, "/Code"), modifiedOnly = F)
