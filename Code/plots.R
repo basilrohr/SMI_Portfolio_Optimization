586bb763a4c881aa762eca255c22eddad4e7b63c
Pascal Aigner <pascal.aigner97@gmail.com>
2020-12-11 12:16:19 +0100

    gg plot themes simplified

--- Code/plots.R
+++ Code/plots.R
@@ -1,23 +1,15 @@
-custom_theme_markdown = theme(axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
-                              axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
-                              plot.title = element_text(margin = margin(t = 0, r = 0, b = 10, l = 0), hjust = 0.5),
-                              panel.background = element_rect(fill = "white", colour = "white"),
-                              panel.grid.major = element_line(colour = "grey", size = 0.4),
-                              panel.grid.minor = element_line(colour = "grey", size = 0.2),
-                              axis.ticks = element_blank(),
-                              text = element_text(family = "Times", size = 15),
-                              legend.key =  element_blank())
-
 custom_theme_shiny = theme(axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
                            axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
-                           plot.title = element_text(margin = margin(t = 0, r = 0, b = 10, l = 0), hjust = 0.5),
+                           plot.title = element_text(margin = margin(t = 0, r = 0, b = 10, l = 0),
+                                                     hjust = 0.5),
                            panel.background = element_rect(fill = "white", colour = "white"),
-                           panel.grid.major = element_line(colour = "grey", size = 0.4),
-                           panel.grid.minor = element_line(colour = "grey", size = 0.2),
+                           panel.grid.major = element_line(colour = "gray", size = 0.4),
+                           panel.grid.minor = element_line(colour = "gray", size = 0.2),
                            axis.ticks = element_blank(),
-                           text = element_text(size = 15),
-                           legend.key =  element_blank())
-
+                           legend.key =  element_blank(),
+                           text = element_text(size = 15))
+                           
+custom_theme_markdown = custom_theme_shiny + theme(text = element_text(family = "Times", size = 15))
 
 gg_cor = function(cor, lab_size, tl.cex, title = " ", theme = NULL) {
   gg = ggcorrplot(cor, lab = T, lab_size = lab_size, tl.cex = tl.cex, title = title, ggtheme = theme) +
